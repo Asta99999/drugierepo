cc0953dab90df57440ee4c06307b80557d7533c6
Asta99999 <64519645+Asta99999@users.noreply.github.com>
2025-03-25 19:49:01 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
+meme_dict = {
+    "Pacior": "Określenie jakiegos smiesznego wydarzenia lub cos co jest smieszne.",
+    "Sigma": "Powiedzenie do kogos gdy cos zrobił smiesznego ale zarazem powaznego.",
+    "XD": "Coś bardzo śmiesznego.",
+}
 
+word = input("Wpisz słowo, którego nie rozumiesz (używaj wielkich liter!): ")
+
+if word in meme_dict:
+    print(meme_dict[word])
+else:
+    print("Nie znaleziono słowa w słowniku.")
